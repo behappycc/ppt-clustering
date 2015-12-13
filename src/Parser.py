7e42b12c699de98e2c7cc56238a343daa9a72c11
behappycc <behappyccc@gmail.com>
2015-12-13 16:48:45 +0800

    modify Parser

--- src/Parser.py
+++ src/Parser.py
@@ -23,6 +23,48 @@ def wordFrequency(words, wordLen, wordTop):
 #string = "台中"
 #print type(string), type(string.decode('utf-8')), string.decode('utf-8')
 
+def readIP(filename, userIP):
+    listData = []
+    listIP = []
+    with open(filename, 'r') as datafile:
+        for line in datafile:
+            listData.append(line)
+    for data in listData:      
+        temp = data.split(' ')
+        if '~' not in temp[1]:
+            school = []
+            school.append(temp[0])
+            school.append(temp[1])
+            school.append(temp[2])
+            listIP.append(school)
+        else:
+            tempIP1 = temp[1].split('.')
+            tempIP2 = tempIP1[2].split('~')
+            for i in xrange(int(tempIP2[0]), int(tempIP2[1])+1):
+                school = []
+                school.append(temp[0])
+                school.append(tempIP1[0] + '.' + tempIP1[1] + '.' + str(i))
+                school.append(temp[2])
+                listIP.append(school)
+
+    listSchool = []
+    for ip in listIP:     
+        if userIP.startswith(ip[1]):
+            listSchool.append(ip)
+            #print ip[0], ip[1]
+    if len(listSchool) == 1:
+        #print listSchool[0][0], listSchool[0][1]
+        return listSchool
+    else:
+        ipLength = 0
+        index = 0
+        for i, j in enumerate(listSchool):
+            if len(j[1]) > ipLength:
+                ipLength = len(j[1])
+                index = i
+        #print listSchool[i][0], listSchool[i][1]
+        return listSchool[i]
+        
 def main():
     sentence = "台中你是一個三寶三寶三寶三寶飯台中！"
     print "Input：", sentence
@@ -34,7 +76,9 @@ def main():
     a = wordFrequency(words, 2, 2)
     print a[0][0], a[0][1]
     print a[1][0], a[1][1]
-    
+
+    school = readIP('school.csv', '140.127.36.8')
+    print school[0][0], school[0][1], school[0][2]
 
 if __name__ == '__main__':
     main()
